3b955a3a2b3149c2dd61e27fc5de0742138cc63b
Siqi Wang <StevW@siqis-air.wv.cc.cmu.edu>
2015-04-27 00:02:24 -0400

    some change to make bootstrap work(possibly)

--- src/main/scala/com/pool/networking/BootstrapMessage.scala
+++ src/main/scala/com/pool/networking/BootstrapMessage.scala
@@ -3,6 +3,7 @@ package com.pool.networking
 /**
  * Created by StevW on 4/20/15.
  */
+@SerialVersionUID(1000L)
 class BootstrapMessage(n : Node, f : Int) extends Serializable{
   var node : Node = n
   var flag : Int = f  //0 for init, 1 for exit
