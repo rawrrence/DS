31beff3d056a92940a2a9e585ef2133f50e2b824
Lawrence <ltsang@andrew.cmu.edu>
2015-04-28 00:27:13 -0400

    Fixed binding issues for RequestDetail

--- src/main/scala/com/pool/RequestDetail.scala
+++ src/main/scala/com/pool/RequestDetail.scala
@@ -1,17 +1,11 @@
 package com.pool
 
 import android.app.Activity
-import android.content.{Context, ComponentName, ServiceConnection, Intent}
+import android.content.{ComponentName, Context, Intent, ServiceConnection}
 import android.graphics.Color
-import android.os.{IBinder, Bundle}
-import android.os.{IBinder, Bundle}
-import android.content.{Context, ComponentName, ServiceConnection, Intent}
-import android.telephony.SmsMessage.MessageClass
-import android.view.View
-import android.view.View.OnClickListener
-import android.widget.{Toast, TextView, Button}
-import com.pool
-import com.pool.networking.Message
+import android.os.{Bundle, IBinder}
+import android.util.Log
+import android.widget.TextView
 
 /**
  * Created by Lawrence on 4/25/15.
@@ -23,6 +17,17 @@ class RequestDetail extends Activity {
   var mConnection : ServiceConnection = new ServiceConnection() {
     def onServiceConnected(className : ComponentName, service : IBinder) : Unit = {
       mBoundService = service.asInstanceOf[NetworkService#LocalBinder].getService()
+
+      var descrpText = findViewById(R.id.orderDetails).asInstanceOf[TextView]
+
+      var requestText = getIntent().getExtras().getString("content")
+      Log.w("Pool", requestText)
+      for(i <- 0 to mBoundService.mp.receivedRequests.size() - 1){
+        Log.w("Pool",mBoundService.mp.receivedRequests.get(mBoundService.mp.receivedRequests.size() - 1 - i).text)
+        if (mBoundService.mp.receivedRequests.get(mBoundService.mp.receivedRequests.size() - 1 - i).text == requestText){
+          descrpText.setText(mBoundService.mp.receivedRequests.get(mBoundService.mp.receivedRequests.size() - 1 - i).text + mBoundService.mp.receivedRequests.get(mBoundService.mp.receivedRequests.size() - 1 - i).mType)
+        }
+      }
     }
     def onServiceDisconnected(className : ComponentName): Unit = {
       mBoundService = null
@@ -40,15 +45,6 @@ class RequestDetail extends Activity {
     setContentView(R.layout.order)
     getWindow().getDecorView().setBackgroundColor(Color.rgb(0,73,105))
 
-    var descrpText = findViewById(R.id.orderDetails).asInstanceOf[TextView]
-
-    var requestText = getIntent().getExtras().getString("content")
-
-    for(i <- 0 to mBoundService.mp.receivedRequests.size() - 1){
-      if (mBoundService.mp.receivedRequests.get(mBoundService.mp.receivedRequests.size() - 1 - i).text == requestText){
-        descrpText.setText(mBoundService.mp.receivedRequests.get(mBoundService.mp.receivedRequests.size() - 1 - i).text + mBoundService.mp.receivedRequests.get(mBoundService.mp.receivedRequests.size() - 1 - i).mType)
-      }
-    }
   }
 
   override def onDestroy(): Unit = {
